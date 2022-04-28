c49efcf141eca884ed319007f886ccc33f31f07e
stanislavtiryoshin <69424907+stanislavtiryoshin@users.noreply.github.com>
2022-04-28 22:34:42 +0600

    Broken beyond repair

--- src/components/Navbar/pages/Simulator.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Simulator = () => {
-  return (
-    <div>Simulator</div>
-  )
-}
--- src/pages/ExchangesPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ExchangesPage = () => {
-  return (
-    <div>Exchanges</div>
-  )
-}
-
-export default ExchangesPage
