bedf22ce02ac063e803c96d48ca4726cdc9d8d5a
stanislavtiryoshin <69424907+stanislavtiryoshin@users.noreply.github.com>
2022-04-28 22:10:06 +0600

    Adjusted imports

---
+++ src/pages/CurrenciesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Cryptocurrencies from '../components/Cryptocurrencies/Cryptocurrencies'
+
+const CurrenciesPage = () => {
+  return (
+    <>
+      <Cryptocurrencies />
+    </>
+  )
+}
+
+export default CurrenciesPage
---
+++ src/pages/ExchangesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExchangesPage = () => {
+  return (
+    <div>Exchanges</div>
+  )
+}
+
+export default ExchangesPage
