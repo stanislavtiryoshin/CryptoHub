9986a4f208b85073c68fda0fa0020f04b6fab0ca
stanislavtiryoshin <69424907+stanislavtiryoshin@users.noreply.github.com>
2022-06-06 19:22:51 +0600

    Add files via upload

---
+++ src/components/Features/Features.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+
+import CurrenciesItem from './items/CurrenciesItem';
+import ExchangesItem from './items/ExchangesItem';
+import NewsItem from './items/NewsItem';
+import SimulatorItem from './items/SimulatroItem';
+
+import './Features.css'
+
+const Features = () => {
+
+  const [item, setItem] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      document.querySelectorAll(".features-item").forEach(feature => {
+        feature.classList.add('active')
+      })
+    }, 100)
+  })
+
+  let displayedItem
+  if (item === 0) displayedItem = <CurrenciesItem />
+  if (item === 1) displayedItem = <NewsItem />
+  if (item === 2) displayedItem = <ExchangesItem />
+  if (item === 3) displayedItem = <SimulatorItem />
+
+  return (
+    <div className="features-box">
+      <h1 className='features-heading'>
+          CryptoHub's Features
+      </h1>
+      <div className="menu-box-wrap">
+        <div className="features-menu-box">
+          {displayedItem}
+        </div>
+      </div>
+      <div className="features-menu">
+          <div className={(item === 0) ? "features-menu-item active" : "features-menu-item"} onClick={() => setItem(0)}>
+            <h3>Currencies</h3>
+          </div>
+          <div className={(item === 1) ? "features-menu-item active" : "features-menu-item"} onClick={() => setItem(1)}>
+            <h3>News</h3>
+          </div>
+          <div className={(item === 2) ? "features-menu-item active" : "features-menu-item"} onClick={() => setItem(2)}>
+            <h3>Exchanges</h3>
+          </div>
+          <div className={(item === 3) ? "features-menu-item active" : "features-menu-item"} onClick={() => setItem(3)}>
+            <h3>Simulator</h3>
+          </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Features
